8e64f0344e5d454fce0b06c08e42f257da6761b3
Daniele Campari <danielecampari@gmail.com>
2022-05-12 23:13:23 +0200

    Add comment to arithmetic function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
-/* Computational functions */
+/* Arithmetic functions */
 
+/**
+* @param {number} a first addend
+* @param {number} b second addend
+* @return {number} sum
+*/
 function add(a, b) {
     return a + b;
 }
 
+/**
+* @param {number} a minuend
+* @param {number} b subtrahend
+* @return {number} difference
+*/
 function substract(a, b) {
     return a - b;
 }
 
+/**
+* @param {number} a first factor
+* @param {number} b second factor
+* @return {number} product
+*/
 function multiply(a, b) {
     return a * b;
 }
 
+/**
+* @param {number} a dividend
+* @param {number} b divisor
+* @return  {number} quotient
+*/
 function divide(a, b) {
     return a / b;
 }
