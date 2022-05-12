0d8291819e09ec18927016e9b0df506d9df4068e
Daniele Campari <danielecampari@gmail.com>
2022-05-12 22:56:08 +0200

    Add computational function

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/* Computational functions */
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+/* Test in console */
+console.log(add(2, 2));
+
+console.log(substract(3, 9));
+
+console.log(multiply(3, 3));
+
+console.log(divide(4, 2));
